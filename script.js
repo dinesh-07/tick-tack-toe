ef5d6856eef7924792b4d5300851b9f8cba177d5
Dinesh Kini <dinesh.kini@oracle.com>
2022-05-09 00:27:56 +0530

    enter x and O logic

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ const containerEle = document.getElementById('container')
 for (let i = 0; i < 9; i++) {
   const block = document.createElement('div')
   block.classList.add('block')
+  block.addEventListener('click', () => blockLogic(block))
   containerEle.appendChild(block)
+}
+let nextPlayer = 'X'
+const blockLogic = (block) => {
+  block.innerHTML = nextPlayer
+  block.style.cursor = 'not-allowed'
+  block.innerHTML = nextPlayer
+  nextPlayer = nextPlayer === 'X' ? 'O' : 'X'
 }
